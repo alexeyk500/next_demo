550c5473f418da2599e50014dda06b06ce47682e
alexeyk500 <alexeyk500@yandex.ru>
2023-09-16 14:43:02 +0300

    Footer Header - done

---
+++ Components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './Header.module.css';
+
+const Header: React.FC = () => {
+    return (
+        <div className={classes.container}>
+          {'Footer'}
+        </div>
+    );
+};
+
+export default Header;
